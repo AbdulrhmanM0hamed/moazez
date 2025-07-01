76249b3e259ae7ce5713d5ace465a058e8b57e33
Abdulrhman Mohammed Shabaan <126196441+AbdulrhmanM0hamed@users.noreply.github.com>
2025-07-01 17:44:03 +0300

    add package name and change name of app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.moazez"
+    namespace = "com.behavtech.moazez"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.moazez"
+        applicationId = "com.behavtech.moazez"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         ndkVersion = "27.0.12077973"
--- android/app/src/main/kotlin/com/behavtech/moazez/MainActivity.kt
+++ android/app/src/main/kotlin/com/behavtech/moazez/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.moazez
+package com.behavtech.moazez
 
 import io.flutter.embedding.android.FlutterActivity
 
